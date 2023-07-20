d820cbb5cca634b40ef94d529fe547b854c3999e
Thierry Marinho <thierrydev48@gmail.com>
2023-07-20 17:10:29 -0300

    Game seed

--- src/main/java/com/example/dslist/models/GameModel.java
+++ src/main/java/com/example/dslist/models/GameModel.java
@@ -25,6 +25,10 @@ public class GameModel {
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 }
